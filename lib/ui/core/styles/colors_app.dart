e781a25ce61f763fee55806508709fb7b3fa5303
LucassMateus <spechtlucasmateus@gmail.com>
2025-02-12 11:49:30 -0300

    feat: update primary color in ColorsApp

--- lib/ui/core/styles/colors_app.dart
+++ lib/ui/core/styles/colors_app.dart
@@ -7,7 +7,8 @@ class ColorsApp {
 
   static ColorsApp get instance => _instance ??= ColorsApp._();
 
-  Color get primary => const Color.fromARGB(255, 194, 28, 28);
+  // Color get primary => const Color.fromARGB(255, 194, 28, 28);
+  Color get primary => const Color(0xFF6D4C41);
   Color get secondary => const Color.fromRGBO(240, 239, 244, 1);
 }
 
